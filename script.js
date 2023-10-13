21d3dee7d638bd501c828d8fece026844a01bcf8
Welnhofer Michael (NuP1/TGA) <wem6nu@bosch.com>
2023-10-13 11:38:33 +0200

    daily update

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let name = prompt("Dein Name");
-alert("Hallo " + name);
+let name = prompt("Dein Name", "Name");
+alert("Hallo dein Name ist " + name.length + " Zeichen lang");
+document.querySelector(".choosen-name").innerHTML = name;
+/* let number1 = prompt("Give me a random number");
+if (number1 % 2 == 0) {
+  alert("Your Number is even");
+} else {
+  alert("Your Number is not even");
+}
+
+let a = 10000;
+let b = 1234;
+let c = -500;
+alert(a + " " + b + " " + c);
+ */
